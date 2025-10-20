05cd4832945a151db33bdf5fa6c1cdf871e3ad20
Osama-285 <osamaaslam285@gmail.com>
2025-10-20 23:13:01 +0500

    container task1

---
+++ jobs/level1_enrichment.py
@@ -0,0 +1,211 @@
+# from pyflink.table import EnvironmentSettings, TableEnvironment
+
+# def main():
+#     settings = EnvironmentSettings.in_streaming_mode()
+#     t_env= TableEnvironment.create(settings)
+#     t_env.get_config().get_configuration().set_string("parallelism.default", "4")
+
+
+#     t_env.execute_sql("""
+#     CREATE TABLE container_events (
+#         containerId STRING,
+#         shipId STRING,
+#         size STRING,
+#         weightKg INT,
+#         sealNumber STRING,
+#         eventType STRING,
+#         location STRING,
+#         status STRING,
+#         `timestamp` TIMESTAMP_LTZ(3),
+#         WATERMARK FOR `timestamp` AS `timestamp` - INTERVAL '60' SECOND
+#     ) WITH (
+#         'connector' = 'kafka',
+#         'topic' = 'containerEvent',
+#         'properties.bootstrap.servers' = 'broker:29094',
+#         'properties.group.id' = 'flink_consumer',
+#         'scan.startup.mode' = 'earliest-offset',
+#         'value.format' = 'json',
+#         'value.json.timestamp-format.standard' = 'ISO-8601',
+#         'value.json.ignore-parse-errors' = 'true'
+#     )
+#     """)
+
+#     t_env.execute_sql("""
+#     CREATE TABLE ship_event (
+#         shipId STRING,
+#         imoNumber STRING,
+#         name STRING,
+#         flag STRING,
+#         capacityTEU INT,
+#         totalContainers INT,
+#         eventType STRING,
+#         berthId STRING,
+#         arrivalTime TIMESTAMP_LTZ(3),
+#         departureTime TIMESTAMP_LTZ(3),
+#         status STRING,
+#         `timestamp` TIMESTAMP_LTZ(3),
+#         WATERMARK FOR `timestamp` AS `timestamp` - INTERVAL '60' SECOND,
+#         PRIMARY KEY (shipId) NOT ENFORCED
+
+#     ) WITH (
+#         'connector' = 'upsert-kafka',
+#         'topic' = 'shipEvent',
+#         'properties.bootstrap.servers' = 'broker:29094',
+#         'key.format' = 'json',
+#         'value.format' = 'json',
+#         'value.json.ignore-parse-errors' = 'true'
+#     )
+#     """)
+#     # t_env.execute_sql("DESCRIBE ship_event").print()
+#     # t_env.execute_sql("DESCRIBE contaner_events").print()
+#     result = t_env.sql_query("""
+#     SELECT
+#         c.containerId,
+#         c.shipId,
+#         c.size,
+#         c.weightKg,
+#         s.capacityTEU,
+#         (CAST(c.weightKg AS DOUBLE) / (s.capacityTEU * 1000.0)) AS utilization_ratio,
+#         c.location,
+#         c.status,
+#         c.`timestamp`
+#         FROM container_events AS c
+#         LEFT JOIN ship_event FOR SYSTEM_TIME AS OF c.`timestamp` AS s
+#         ON c.shipId = s.shipId
+#     """)
+#     t_env.execute_sql("""
+#     CREATE TABLE enriched_sink (
+#         containerId STRING,
+#         shipId STRING,
+#         size STRING,
+#         weightKg INT,
+#         capacityTEU INT,
+#         utilization_ratio DOUBLE,
+#         location STRING,
+#         status STRING,
+#         ts TIMESTAMP(3)
+#         ) WITH (
+#         'connector' = 'print'
+#         )
+#     """)
+#     # result.print_schema()
+#     t_env.execute_sql("DESCRIBE ship_event").print()
+#     t_env.execute_sql("SHOW CREATE TABLE ship_event").print()
+#     result.execute_insert("enriched_sink").print()
+
+
+# if __name__ == "__main__":
+#     main()
+
+from pyflink.table import EnvironmentSettings, TableEnvironment
+
+def main():
+    # -------------------- Environment --------------------
+    settings = EnvironmentSettings.in_streaming_mode()
+    t_env = TableEnvironment.create(settings)
+
+    # -------------------- Source 1: Container Events --------------------
+    t_env.execute_sql("""
+    CREATE TABLE container_events (
+        containerId STRING,
+        shipId STRING,
+        size STRING,
+        weightKg DOUBLE,
+        sealNumber STRING,
+        eventType STRING,
+        location STRING,
+        status STRING,
+        `timestamp` TIMESTAMP_LTZ(3),
+        WATERMARK FOR `timestamp` AS `timestamp` - INTERVAL '60' SECOND
+    ) WITH (
+        'connector' = 'kafka',
+        'topic' = 'containerEvent',
+        'properties.bootstrap.servers' = 'broker:29094',
+        'properties.group.id' = 'flink-container-enrich',
+        'format' = 'json',
+        'scan.startup.mode' = 'earliest-offset'
+    )
+    """)
+
+    # -------------------- Source 2: Ship Events --------------------
+    t_env.execute_sql("""
+    CREATE TABLE ship_events (
+        shipId STRING,
+        imoNumber STRING,
+        name STRING,
+        flag STRING,
+        capacityTEU INT,
+        totalContainers INT,
+        eventType STRING,
+        berthId STRING,
+        arrivalTime TIMESTAMP_LTZ(3),
+        departureTime TIMESTAMP_LTZ(3),
+        status STRING,
+        `timestamp` TIMESTAMP_LTZ(3),
+        WATERMARK FOR `timestamp` AS `timestamp` - INTERVAL '60' SECOND,
+        PRIMARY KEY (shipId) NOT ENFORCED
+    ) WITH (
+        'connector' = 'upsert-kafka',
+        'topic' = 'shipEvent',
+        'properties.bootstrap.servers' = 'broker:29094',
+        'key.format' = 'json',
+        'value.format' = 'json',
+        'value.fields-include' = 'ALL'
+    )
+    """)
+
+    # -------------------- Convert Ship Stream to Versioned Table --------------------
+    # (Allows time-based join using FOR SYSTEM_TIME AS OF)
+    t_env.execute_sql("""
+    CREATE VIEW ship_meta AS
+    SELECT
+        shipId,
+        imoNumber,
+        name,
+        flag,
+        capacityTEU,
+        totalContainers,
+        status,
+        `timestamp` AS rowtime
+    FROM ship_events
+    """)
+
+    # -------------------- Sink: Print Enriched Results --------------------
+    t_env.execute_sql("""
+    CREATE TABLE print_sink (
+        containerId STRING,
+        shipId STRING,
+        size STRING,
+        weightKg DOUBLE,
+        capacityTEU DOUBLE,
+        utilization_ratio DOUBLE,
+        shipStatus STRING,
+        location STRING,
+        containerStatus STRING,
+        `timestamp` TIMESTAMP_LTZ(3)
+    ) WITH (
+        'connector' = 'print'
+    )
+    """)
+
+    # -------------------- Enrichment Query --------------------
+    result = t_env.execute_sql("""
+    INSERT INTO print_sink
+    SELECT
+        c.containerId,
+        c.shipId,
+        c.size,
+        c.weightKg,
+        s.capacityTEU,
+        (c.weightKg / (s.capacityTEU * 1000.0)) AS utilization_ratio,
+        s.status AS shipStatus,
+        c.location,
+        c.status AS containerStatus,
+        c.`timestamp`
+    FROM container_events AS c
+    LEFT JOIN ship_meta FOR SYSTEM_TIME AS OF c.`timestamp` AS s
+      ON c.shipId = s.shipId
+    """)
+    result.print()
+if __name__ == "__main__":
+    main()
